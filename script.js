17ad10b601bf5d53a8149c3f3391fe8fcaa19b6a
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-26 17:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+let h3El = document.getElementById("url");
+document.querySelector(".form").addEventListener(('submit'),(e)=>{
+	e.preventDefault();
+	let name = document.getElementById("name").value;
+	let year = document.getElementById("year").value;
+
+	if(name === ""){
+		h3El.textContent = `https://localhost:8080/?year=${year}`
+	}
+	else if(year === ""){
+		h3El.textContent = `https://localhost:8080/?name=${name}`
+	}
+	else if(name === "" && year === ""){
+		h3El.textContent = `https://localhost:8080/`
+	}
+	else{
+		h3El.textContent = `https://localhost:8080/?name=${name}&year=${year}`
+	}
+})
